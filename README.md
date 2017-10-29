1ec1f300fb910d4e77cab9110b008df175983cb7
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-10-29 17:26:54 +0000

    adding GC

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gsql
+
+```javascript
+
+Using the MySQL Go Driver, and testing GC!
+mem.Alloc:  73016
+mem.TotalAlloc:  73016
+mem.HeapAlloc:  73016
+mem.NumGC:  0
+------
+mem.Alloc:  100074208
+mem.TotalAlloc:  1000153432
+mem.HeapAlloc:  100074208
+mem.NumGC:  10
+------
+mem.Alloc:  66760
+mem.TotalAlloc:  2000234568
+mem.HeapAlloc:  66760
+mem.NumGC:  20
+------
+```
--- main.go
+++ main.go
@@ -2,10 +2,47 @@ package main
 
 import (
     "fmt"
+    "runtime"
+    "time"
     _ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-    fmt.Println("Using the MySQL Go Driver!")
+func printStats(mem runtime.MemStats) {
+    
+    runtime.ReadMemStats(&mem)
+    
+    fmt.Println("mem.Alloc: ", mem.Alloc)
+    fmt.Println("mem.TotalAlloc: ", mem.TotalAlloc)
+    fmt.Println("mem.HeapAlloc: ", mem.HeapAlloc)
+    fmt.Println("mem.NumGC: ", mem.NumGC)
+    fmt.Println("------")
 }
+
+func main() {
+    fmt.Println("Using the MySQL Go Driver, and testing GC!")
+    
+    var mem runtime.MemStats
+    printStats(mem)
+
+    for i := 0; i< 10; i++ {
+        s := make([]byte, 100000000)
+        if s == nil {
+           fmt.Println("Operation failed")
+        }
+     }
+
+
+     printStats(mem)
+
+      for i := 0; i< 10; i++ {
+         s := make([]byte, 100000000)
+         if s == nil {
+            fmt.Println("Operation failed")
+         }
+         time.Sleep(5 * time.Second)
+    }
+   printStats(mem)
+}
+
+
 
